b2a5a667610397013d5b32835827f1cb7f2c6139
MosxLab <georgemosxolios@gmail.com>
2026-06-13 22:46:16 +0300

    Delete practice-exercises-02 directory

--- practice-exercises-02/ask10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int x , y , z , S;
-    scanf("%o%x%d", &x , &y , &z);
-    S = (int)(x+y+z);
-    printf("Athroisma:%d\n" , S);
-    return 0;
-}
--- practice-exercises-02/ask5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-  float x ;
-  scanf("%f", &x);
-  printf("%d\n",(int)x);
-  printf("%d",(int)x+1);
- return 0;
-}
